bc7449e168a76101065cf6b7ec1b19e94d642a07
Janne Mattila <janne.mattila@hotmail.fi>
2023-05-31 18:23:18 +0300

    Add simple network validation script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# network-test-scripts
+# Network test scripts
+
 Network test scripts
+
+## AKS
+
+```bash
+curl -L https://raw.githubusercontent.com/JanneMattila/network-test-scripts/main/aks.sh | bash
+```
---
+++ aks.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+echo "|-------------------------------------------------------|"
+echo "|             _    _  ______                            |"
+echo "|            / \  | |/ / ___|                           |"
+echo "|           / _ \ | ' /\___ \                           |"
+echo "|          / ___ \| . \ ___) |                          |"
+echo "|         /_/   \_\_|\_\____/                           |"
+echo "| Checking connectivity to required ports and addresses |"
+echo "|                                                       |"
+echo "| https://aka.ms/aks-required-ports-and-addresses       |"
+echo "|-------------------------------------------------------|"
+
+region="northeurope"
+summary=""
+failures=0
+
+function test_connection()
+{
+    local address="$1"
+    local port="$2"
+    local additional_switches="$3"
+
+    echo ""
+    nc -zv -w 3 $additional_switches $address $port
+    nc_exit_code=$?
+    if [ "$nc_exit_code" -ne 0 ]; then
+        echo "Unable to connect to $address:$port"
+        summary="$summary\nERROR - $address:$port"
+        failures=$((failures + 1)) 
+    else
+        echo "Connected to $address:$port"
+        summary="$summary\nOK    - $address:$port"
+    fi
+}
+
+test_connection mcr.microsoft.com 443
+test_connection $region.data.mcr.microsoft.com 443
+test_connection management.azure.com 443
+test_connection packages.microsoft.com 443
+test_connection esm.ubuntu.com 443
+test_connection security.ubuntu.com 80
+test_connection azure.archive.ubuntu.com 80
+test_connection changelogs.ubuntu.com 80
+test_connection login.microsoft.com 443
+test_connection ntp.ubuntu.com 123 -u
+test_connection 1.1.1.1 53 -u
+
+echo ""
+echo "|-----------------------------------------------------|"
+echo "|  ____                                               |"
+echo "| / ___| _   _ _ __ ___  _ __ ___   __ _ _ __ _   _   |"
+echo "| \\___ \\| | | | '_ \` _ \\| '_ \` _ \\ / _\` | '__| | | |  |"
+echo "|  ___) | |_| | | | | | | | | | | | (_| | |  | |_| |  |"
+echo "| |____/ \__,_|_| |_| |_|_| |_| |_|\__,_|_|   \__, |  |"
+echo "|                                             |___/   |"
+echo "|-----------------------------------------------------|"
+echo -e $summary
+echo ""
+
+if [ "$failures" -ne 0 ]; then
+    echo ""
+    echo "$failures connectivity checks failed."
+    echo ""
+    echo "You *NEED* to open the ports and addresses documented here:"
+    echo ""
+    echo "https://aka.ms/aks-required-ports-and-addresses"
+    echo ""
+    exit 1
+fi
+
+echo "All connectivity checks passed."
+echo ""
+exit 0
